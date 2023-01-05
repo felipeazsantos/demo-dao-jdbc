3d40fab19137fa55a7e56c7918fa2676fef3b66b
Felipe Azevedo <felipeasantos10@gmail.com>
2023-01-05 17:17:14 -0300

    findbyId implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -1,7 +1,91 @@
 package model.dao.impl;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import db.DB;
+import db.DbException;
 import model.dao.SellerDao;
+import model.entities.Department;
+import model.entities.Seller;
 
 public class SellerDaoJDBC implements SellerDao {
+
+	private Connection conn;
+	
+	public SellerDaoJDBC(Connection conn) {
+		this.conn = conn;
+	}
+	
+	@Override
+	public void insert(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update(Seller obj) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Seller findById(Integer id) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+			
+			String sql = "SELECT seller.*, department.Name as DepName "
+						 + "FROM seller INNER JOIN department "
+						 + "ON seller.DepartmentId = department.Id "
+						 + "WHERE seller.Id = ?";
+			
+			st = conn.prepareStatement(sql);
+			
+			st.setInt(1, id);
+			
+			rs = st.executeQuery();
+			
+			if (rs.next()) {
+				Department department = new Department(rs.getInt("DepartmentId"), rs.getString("DepName"));
+				Seller seller = new Seller(
+						rs.getInt("Id"), 
+						rs.getString("Name"), 
+						rs.getString("Email"), 
+						rs.getDate("BirthDate"), 
+						rs.getDouble("BaseSalary"),
+						department);
+				
+				return seller;
+			} 
+			
+			return null;
+			
+			
+		} catch (SQLException e) {
+			throw new DbException(e.getMessage());
+		} finally {
+			DB.closeResultSet(rs);
+			DB.closeStatement(st);
+		}
+		
+		
+	}
+
+	@Override
+	public List<Seller> findAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
